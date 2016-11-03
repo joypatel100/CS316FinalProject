b9f738f86c317d76af116fc0c79ef114303cc9bf
Joy Patel <joypatel0123@gmail.com>
2016-11-03 13:49:23 -0400

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projectApp
+ * @description
+ * # projectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projectApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial',
+    'ngMessages',
+    'ngResource'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
