67463643b7ba56be2808f99b53c87925b6162697
Joy Patel <joypatel0123@gmail.com>
2016-11-11 13:35:16 -0500

    Beautified code.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,38 +8,38 @@
  * Controller of the projectApp
  */
 angular.module('projectApp')
-  .controller('MainCtrl', [ '$scope', 'DBService', function ($scope, DBService) {
+  .controller('MainCtrl', ['$scope', 'DBService', function($scope, DBService) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-        var self = this;
-        self.showBasic = false;
+    var self = this;
+    self.showBasic = false;
 
-        // options for the types of analytics offered
-        self.analytics = ['Basic'];
+    // options for the types of analytics offered
+    self.analytics = ['Basic'];
 
-        // take the form data and create an API request
-        self.submit = function() {
-                self.speech1 = $scope.speeches.first;
-                self.speech2 = $scope.speeches.second;
+    // take the form data and create an API request
+    self.submit = function() {
+      self.speech1 = $scope.speeches.first;
+      self.speech2 = $scope.speeches.second;
 
-                if ($scope.speeches.analytics === 'Basic') {
-                        DBService.getBasic($scope.speeches).then(function(promise){
-                          console.log(promise);
-                          self.displayBasic(promise.data);
-                        });
-                }
-        };
+      if ($scope.speeches.analytics === 'Basic') {
+        DBService.getBasic($scope.speeches).then(function(promise) {
+          console.log(promise);
+          self.displayBasic(promise.data);
+        });
+      }
+    };
 
-        // "Basic" analytics -- displays the plain text comparison score
-        self.displayBasic = function(data) {
-                self.score = data.score;
-                self.showBasic = true;
-                console.log($scope.basic);
-        };
+    // "Basic" analytics -- displays the plain text comparison score
+    self.displayBasic = function(data) {
+      self.score = data.score;
+      self.showBasic = true;
+      console.log($scope.basic);
+    };
 
 
   }]);
--- app/scripts/services/dbservice.js
+++ app/scripts/services/dbservice.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('projectApp')
-  .factory('DBService',['$http', '$q', 'Config',
+  .factory('DBService', ['$http', '$q', 'Config',
     function DBService($http, $q, Config) {
       var service = {};
 
-                //
+      //
       service.getBasic = function(speeches) {
         var deferred = $q.defer();
 
         $http({
           method: 'GET',
           url: Config.baseUrl + 'speechassociation?' +
-                'speech_id1=' + speeches.first + '&' +
-                'speech_id2=' + speeches.second
-        }).then(function successCallback(response){
+            'speech_id1=' + speeches.first + '&' +
+            'speech_id2=' + speeches.second
+        }).then(function successCallback(response) {
           console.log(response);
           deferred.resolve({
             data: response.data.data[0]
@@ -23,9 +23,10 @@ angular.module('projectApp')
           deferred.reject();
           console.log(response);
         });
-                          // deferred.resolve({'success': true, 'data': {'speech1': '1', 'speaker1': 'josh', 'speech2': '2', 'speaker2': 'sarah', 'association': 0.5}});
+        // deferred.resolve({'success': true, 'data': {'speech1': '1', 'speaker1': 'josh', 'speech2': '2', 'speaker2': 'sarah', 'association': 0.5}});
         return deferred.promise;
       };
 
       return service;
-}]);
+    }
+  ]);
