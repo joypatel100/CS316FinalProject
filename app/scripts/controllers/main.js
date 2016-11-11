e15b9d555517677d55093e0d132be3f1e4258cc1
Joy Patel <joypatel0123@gmail.com>
2016-11-11 02:52:05 -0500

    Added Dockerfile and database routes.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the projectApp
  */
 angular.module('projectApp')
-  .controller('MainCtrl', [ '$scope', 'CompareService', function ($scope, CompareService) {
+  .controller('MainCtrl', [ '$scope', 'DBService', function ($scope, DBService) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -17,7 +17,7 @@ angular.module('projectApp')
 
         var self = this;
         self.showBasic = false;
-     
+
         // options for the types of analytics offered
         self.analytics = ['Basic'];
 
@@ -27,10 +27,11 @@ angular.module('projectApp')
                 self.speech2 = $scope.speeches.second;
 
                 if ($scope.speeches.analytics === 'Basic') {
-                        CompareService.getBasic($scope.speeches).then(function(data){
-                                        self.displayBasic(data.data);       
+                        DBService.getBasic($scope.speeches).then(function(promise){
+                          console.log(promise);
+                          self.displayBasic(promise.data);
                         });
-                } 
+                }
         };
 
         // "Basic" analytics -- displays the plain text comparison score
