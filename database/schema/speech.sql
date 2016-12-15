4ad38189d67355de940db5df30171d5b7f6e8193
Gautam Hathi <gkh8@duke.edu>
2016-12-14 23:00:09 -0500

    updating speech sql

--- database/schema/speech.sql
+++ database/schema/speech.sql
@@ -3,5 +3,9 @@ CREATE TABLE IF NOT EXISTS Speech
  speech_text VARCHAR(100000) NOT NULL,
  date_said date NOT NULL,
  speaker_id INTEGER NOT NULL REFERENCES Speaker(speaker_id),
- username VARCHAR(256) NOT NULL REFERENCES Users(username)
+ username VARCHAR(256) NOT NULL REFERENCES Users(username),
+ wordcount INTEGER,
+ keywords TEXT,
+ sentiment DOUBLE PRECISION,
+ subjectivity DOUBLE PRECISION
 );
